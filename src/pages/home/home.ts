830a1db948e6db73c1abdd6ac74a6f5b3399bb87
oussou-dev <oussou.dev@gmail.com>
2017-12-18 11:40:22 +0100

    Product List on Home Page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,15 +12,23 @@ export class HomePage {
 
   WooCommerce: any;
   products: any[];
+  moreProducts: any[];
+  page: number;
 
   @ViewChild('productSlides') productSlides: Slides
 
   constructor(public navCtrl: NavController) {
+    
+    this.page = 2;
+
     this.WooCommerce = WC({
         url: "http://samarth.southeastasia.cloudapp.azure.com",
         consumerKey: "ck_a55da2f5918a380ed8565ba180fb04f4ec67f304",
         consumerSecret: "cs_3a5776160220af80f004a6983942fc5e06de22a4"
     })
+
+    // appel de la function d'implementation de l'Infinite Scroll
+    this.loadMoreProducts()
 
     // obtenir les donnees de woocommerce store
     this.WooCommerce.getAsync("products").then((data) => { // en retour on a une Promise
@@ -41,4 +49,13 @@ export class HomePage {
    }, 3000)
   }
 
+  loadMoreProducts() {
+      // obtenir les donnees de woocommerce store de la page ? & Infinite Scroll
+    this.WooCommerce.getAsync("products?page=" + this.page).then((data) => { // en retour on a une Promise
+      console.log(JSON.parse(data.body))
+      this.moreProducts = JSON.parse(data.body).products 
+    }, (err) => {
+      console.log(err)
+    })
+  }
 }
