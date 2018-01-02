3ccf77d7fde6232abe6c8fc2f710a4b423940c8a
oussou-dev <oussou.dev@gmail.com>
2018-01-03 00:43:23 +0100

    Displaying Product Specs

--- src/pages/products-by-category/products-by-category.ts
+++ src/pages/products-by-category/products-by-category.ts
@@ -1,43 +1,48 @@
+import { ProductDetailsPage } from './../product-details/product-details';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import * as WC from 'woocommerce-api';
 
+
 @Component({
-  selector: 'page-products-by-category',
-  templateUrl: 'products-by-category.html',
+  selector: "page-products-by-category",
+  templateUrl: "products-by-category.html"
 })
 export class ProductsByCategoryPage {
-
   WooCommerce: any;
   products: any[];
   page: number;
   category: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  
     this.page = 1;
 
-    this.category = this.navParams.get("category")
+    this.category = this.navParams.get("category");
 
     this.WooCommerce = WC({
       url: "http://samarth.southeastasia.cloudapp.azure.com",
       consumerKey: "ck_a55da2f5918a380ed8565ba180fb04f4ec67f304",
       consumerSecret: "cs_3a5776160220af80f004a6983942fc5e06de22a4"
     });
-  
+
     // filtrer les produits obtenus depuis le store par catégorie
     // 2 categories peuvent avoir le même nom mais pas le même slug dans woocommerce
-    this.WooCommerce.getAsync("products?filter[category]=" + this.category.slug).then((data) => { // en retour on a une Promise
-      console.log(JSON.parse(data.body))
-      this.products = JSON.parse(data.body).products 
-    }, (err) => {
-      console.log(err)
-    })
-
+    this.WooCommerce.getAsync(
+      "products?filter[category]=" + this.category.slug
+    ).then(
+      data => {
+        // en retour on a une Promise
+        console.log(JSON.parse(data.body));
+        this.products = JSON.parse(data.body).products;
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProductsByCategoryPage');
+    console.log("ionViewDidLoad ProductsByCategoryPage");
   }
 
   loadMoreProducts(event) {
@@ -45,21 +50,23 @@ export class ProductsByCategoryPage {
     this.page++;
 
     console.log("Getting page " + this.page);
-    this.WooCommerce.getAsync("products?filter[category]=" + this.category.slug + "&page=" + this.page).then((data) => {
-      let temp = (JSON.parse(data.body).products)
+    this.WooCommerce.getAsync(
+      "products?filter[category]=" + this.category.slug + "&page=" + this.page
+    ).then(data => {
+      let temp = JSON.parse(data.body).products;
 
-      this.products = this.products.concat(JSON.parse(data.body).products)
-      console.log(this.products)
+      this.products = this.products.concat(JSON.parse(data.body).products);
+      console.log(this.products);
       event.complete();
 
       if (temp.length < 10) {
-        event.enable(false)
+        event.enable(false);
       }
-    })
-
+    });
   }
 
-
-
-
+  openProductPage(product) {
+    // push() adds the new page on the top of the navigation stack with the option to going back
+    this.navCtrl.push(ProductDetailsPage, { product: product });
+  }
 }
