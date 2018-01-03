1a63b1a4bd7e9b854e01dc7c0a7a68832215f55f
oussou-dev <oussou.dev@gmail.com>
2018-01-03 01:26:03 +0100

    Displaying Product Reviews

--- src/pages/product-details/product-details.ts
+++ src/pages/product-details/product-details.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import * as WC from 'woocommerce-api'
 
 @Component({
   selector: 'page-product-details',
@@ -9,6 +9,8 @@ import { NavController, NavParams } from 'ionic-angular';
 export class ProductDetailsPage {
 
   product: any;
+  WooCommerce: any;
+  reviews: any[] = [];
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams) {
@@ -16,6 +18,20 @@ export class ProductDetailsPage {
     this.product = this.navParams.get("product")
     console.log(this.product)
   
+    this.WooCommerce = WC({
+      url: "http://samarth.southeastasia.cloudapp.azure.com",
+      consumerKey: "ck_a55da2f5918a380ed8565ba180fb04f4ec67f304",
+      consumerSecret: "cs_3a5776160220af80f004a6983942fc5e06de22a4"
+    });  
+
+    this.WooCommerce.getAsync('products/'+this.product.id+'/reviews').then((data) => {
+      this.reviews = JSON.parse(data.body).product_reviews;
+      console.log(this.reviews)
+    }, (err) => {
+      console.log(err)
+    })
+
+
   }
 
   ionViewDidLoad() {
