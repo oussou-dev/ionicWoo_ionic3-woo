23d11b8338718e64b9b100335cc05de44a8bee47
oussou-dev <oussou.dev@gmail.com>
2018-01-05 15:50:55 +0100

    Implementing Cart using Storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { Menu } from '../pages/menu/menu';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { IonicStorageModule } from "@ionic/storage";
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -22,6 +25,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -34,7 +38,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
--- src/pages/product-details/product-details.ts
+++ src/pages/product-details/product-details.ts
@@ -1,41 +1,94 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ToastController } from 'ionic-angular';
 import * as WC from 'woocommerce-api'
+import { Storage } from '@ionic/storage';
+
 
 @Component({
-  selector: 'page-product-details',
-  templateUrl: 'product-details.html',
+  selector: "page-product-details",
+  templateUrl: "product-details.html"
 })
 export class ProductDetailsPage {
-
   product: any;
   WooCommerce: any;
   reviews: any[] = [];
 
-  constructor(public navCtrl: NavController, 
-              public navParams: NavParams) {
-    
-    this.product = this.navParams.get("product")
-    console.log(this.product)
-  
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public storage: Storage,
+    private toastCtrl: ToastController
+  ) {
+    this.product = this.navParams.get("product");
+    console.log(this.product);
+
     this.WooCommerce = WC({
       url: "http://samarth.southeastasia.cloudapp.azure.com",
       consumerKey: "ck_a55da2f5918a380ed8565ba180fb04f4ec67f304",
       consumerSecret: "cs_3a5776160220af80f004a6983942fc5e06de22a4"
-    });  
+    });
 
-    this.WooCommerce.getAsync('products/'+this.product.id+'/reviews').then((data) => {
-      this.reviews = JSON.parse(data.body).product_reviews;
-      console.log(this.reviews)
-    }, (err) => {
-      console.log(err)
-    })
-
-
+    this.WooCommerce.getAsync("products/" + this.product.id + "/reviews").then(
+      data => {
+        this.reviews = JSON.parse(data.body).product_reviews;
+        console.log(this.reviews);
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProductDetailsPage');
+    console.log("ionViewDidLoad ProductDetailsPage");
   }
 
+  addToCart(product) {
+    // get a key/value pair
+    this.storage.get("cart").then(data => {
+      if (data == null || data.length == 0) {
+        data = [];
+        data.push({
+          product: product,
+          qty: 1,
+          amount: parseFloat(product.price)
+        });
+      } else {
+        let added = 0;
+
+        for (let i = 0; i < data.length; i++) {
+          if (product.id == data[i].product.id) {
+            console.log("Product is already in the cart");
+
+            let qty = data[i].qty;
+            data[i].qty = qty + 1;
+            data[i].amount =
+              parseFloat(data[i].amount) + parseFloat(data[i].product.price);
+            added = 1;
+          }
+        }
+
+        if (added == 0) {
+          data.push({
+            product: product,
+            qty: 1,
+            amount: parseFloat(product.price)
+          });
+        }
+      }
+
+      // update the cart to the localstorage
+      this.storage.set("cart", data).then(() => {
+        console.log("Cart Updated");
+        console.log(data);
+
+        this.toastCtrl
+          .create({
+            message: "Cart Updated",
+            duration: 3000
+          })
+          .present();
+      });
+    });
+  }
 }
