0e821a16ffe9d595180bac7acb0b707fbe6d83b7
oussou-dev <oussou.dev@gmail.com>
2018-01-05 16:49:38 +0100

    Displaying Products in the Cart

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,5 +1,7 @@
+import { Storage } from '@ionic/storage';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-cart',
@@ -7,7 +9,21 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class CartPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  cartItems: any[] = [];
+  total: any;
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              private storage: Storage
+            ) {
+      this.storage.ready().then(() => {
+        this.storage.get("cart").then((data) => {
+          this.cartItems = data
+          console.log(data);
+          
+        })
+      })
+    
   }
 
   ionViewDidLoad() {
