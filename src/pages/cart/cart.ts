06204673eea95ad18ed2042f69853bdb467f4c16
oussou-dev <oussou.dev@gmail.com>
2018-01-06 20:14:18 +0100

    Finilizing Cart

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,6 +1,6 @@
 import { Storage } from '@ionic/storage';
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ViewController } from 'ionic-angular';
 
 
 @Component({
@@ -11,16 +11,29 @@ export class CartPage {
 
   cartItems: any[] = [];
   total: any;
+  showEmptyCartMessage: boolean
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
-              private storage: Storage
+              private storage: Storage,
+              private viewCtrl: ViewController
             ) {
+
+      this.total = 0.0
+
       this.storage.ready().then(() => {
         this.storage.get("cart").then((data) => {
           this.cartItems = data
-          console.log(data);
+          console.log(this.cartItems);
           
+          if (this.cartItems.length > 0) {
+            
+            this.cartItems.forEach((item, index) => {
+              this.total = this.total + (item.product.price * item.qty)
+            })
+          } else {
+            this.showEmptyCartMessage = true
+          }
         })
       })
     
@@ -28,6 +41,28 @@ export class CartPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CartPage');
+  }
+
+  // remove product from cart
+  // supprimer un produit + mettre à jour le total à payer + prévoir le cas où le produit supprimé est le dernier
+  removeFromCart(item, i) {
+    let price = item.product.price
+    let qty = item.qty
+
+    this.cartItems.splice(i,1);
+
+    this.storage.set("cart", this.cartItems).then( () => {
+      this.total = this.total - (price * qty)
+    })
+
+    if (this.cartItems.length == 0) {
+      this.showEmptyCartMessage = true
+    } 
+    
+  }
+
+  closeModal() {
+    this.viewCtrl.dismiss()
   }
 
 }
